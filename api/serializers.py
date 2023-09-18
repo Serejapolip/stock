b9b790a4ab4f1632f437a796fa191de1b3925f83
Sergey <nttregubm@ya.ru>
2023-09-18 16:27:33 +0300

    startapp api

---
+++ api/serializers.py
@@ -0,0 +1,60 @@
+from django.core.validators import MaxValueValidator
+from rest_framework import serializers, request
+from rest_framework import validators
+
+from rest_framework.fields import ChoiceField
+from rest_framework.relations import PrimaryKeyRelatedField, SlugRelatedField
+
+from api.models import ApiUser, Stock, Product, Business
+
+
+class UserSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=128, validators=[
+        validators.UniqueValidator(ApiUser.objects.all())
+    ])
+    email = serializers.EmailField(validators=[
+        validators.UniqueValidator(ApiUser.objects.all())
+    ])
+    password = serializers.CharField(min_length=6, max_length=20, write_only=True)
+    user_type = serializers.ChoiceField(choices=ApiUser.user_type_choices)
+
+    def create(self, validated_data):
+        user = ApiUser.objects.create(
+            email=validated_data["email"],
+            username=validated_data["username"],
+            user_type=validated_data['user_type']
+        )
+        user.set_password(validated_data["password"])
+        user.save(update_fields=["password"])
+        return user
+
+
+class StockSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Stock
+        fields = "__all__"
+        extra_kwargs = {"id": {"read_only": True}}
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    owner = serializers.HiddenField(
+        default=serializers.CurrentUserDefault()
+    )
+    class Meta:
+        model = Product
+        fields = "__all__"
+        extra_kwargs = {"id": {"read_only": True}}
+
+
+class BusinessSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(
+        default=serializers.CurrentUserDefault()
+    )
+
+    quantity = serializers.IntegerField(min_value=1)
+
+    class Meta:
+        model = Business
+        fields = "__all__"
+        extra_kwargs = {"id": {"read_only": True}}
--- stock/settings.py
+++ stock/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'api.ApiUser'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ]
+}
