adaaa0b13c609a80bf829634b96989c6341108b9
Serejapolip <130852066+Serejapolip@users.noreply.github.com>
2024-02-23 20:24:17 +0300

    Update settings.py

--- stock/settings.py
+++ stock/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q1ce&76m5d0m1fo(sktfx01urv3m7rymr&(gvkwwbhij$!t46t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['serejapolip.pythonanywhere.com']
 
 
 # Application definition
